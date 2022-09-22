e479687c9c2cf191e6b5cbb80b8911b7763ca24f
boson13579 <boson13579@gmail.com>
2022-09-22 13:28:45 +0800

    uva10800

--- test.cpp
+++ test.cpp
@@ -1,59 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> a(vector<vector<int>> *matrix){
-	vector<vector<int>> temp = *matrix;
-	temp[1][1] = 1;
-	return temp;
-}
+map <int,vector<pair<int,int>> , greater<int>> qq;
 
-
-int factorsum(int a){
-	map<int,int> factor;
-	int divide = 2;
-	while(a!=1){
-		if(a%divide==0){
-			if(factor.find(divide)==factor.end())
-				factor.insert(make_pair(divide,1));
-			else
-				factor[divide] = factor[divide]+1;
-			a/=divide;
-			continue;
-		}
-		divide++;	
-	}
-	int sum=1;
-	for(auto i:factor){
-		cout<<i.first<<" "<<i.second<<"\n";
-		sum*=((i.first*(((i.second*i.second+i.second)/2)+1)));
-	}
-		
-	return sum;
-}
 int main () {
 	//freopen("test.in", "r", stdin);
 	//freopen("test.out","w", stdout);
 	ios::sync_with_stdio(0), cin.tie(0);
 
-	cout<<factorsum(5)<<"\n";
-	// vector<vector<int>> qq;
-	// for(int i=0 ; i<2 ; i++){
-	// 	vector<int> temp;
-	// 	for(int t=0 ; t<2 ; t++){
-	// 		int tempint;
-	// 		cin>>tempint;
-	// 		temp.emplace_back(tempint);
-	// 	}
-	// 	qq.emplace_back(temp);
-	// }
-	// qq=a(&qq);
-	// for(auto i:qq){
-	// 	for(auto t:i){
-	// 		cout<<t<<" ";
-	// 	}
-	// 	cout<<"\n";
-	// }
+	vector<pair<int,int>> temp;
+	temp.emplace_back(0,1);
+	qq.insert(make_pair(50,temp));
+	temp.clear();
+	temp.emplace_back(make_pair(1,0));
+	qq.insert(make_pair(51,temp));
+	temp.clear();
+	qq[50].emplace_back(make_pair(2,-1));
 
+	for(auto i=qq.begin() ; i!=qq.end() ; i++){
+		cout<<"| ";
+		int nowCol = 0;
+		for(auto t:(*i).second){
+			for(int a=nowCol ; a<t.first ; a++){
+				cout<<" ";
+			}
+			if(t.second==1)
+				cout<<"/";
+			else if(t.second==0)
+				cout<<"_";
+			else if(t.second==-1)
+				cout<<"\\";
+		}
+		cout<<"\n";
+	}
+	cout<<"+";
+	for(int i=0 ; i<3 ; i++)
+		cout<<"-";
 
 	return 0;
 }
