2efc5ce209f7722bde74cb50b8d3e9edf7b0e633
boson13579 <boson13579@gmail.com>
2023-01-16 22:28:33 +0800

    pratice

---
+++ C++/CSES/Dynamic_Programming/Dice_Combinations/Dice_Combinations.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+DP[1000005];
+
+int dfs(int n){
+
+	if(DP[n]!=0 or n==0)	return DP[n];
+
+	int sum=0;
+
+	for(int i=1 ; i<7;i++){
+		
+	}
+
+
+}
+
+
+int main () {
+	//freopen("test.in", "r", stdin);
+	//freopen("test.out","w", stdout);
+	ios::sync_with_stdio(0), cin.tie(0);
+
+	DP[0] = 0;
+	DP[1] = 1;
+	DP[2] = 2;
+	
+	int a;
+	cin>>a;
+	
+
+
+	return 0;
+}
+
--- test.cpp
+++ test.cpp
@@ -1,16 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-int data[30][30];
-bool isFull(int a, int b, int c,int d){
-	for(int i=a;i<=c;i++){
-		for(int j=b;j<=d;j++){
-			if(!data[i][j]) return false;
-		}
-	}
-	return true;
-}
+
+
+set<int> q;
+
 int main() {
 	int a;
-	cout<<a<<endl;
+	cout<<*q.begin()<<endl;
 	return 0;
 }
