6ef5ceddb3690d865d369a54ec925f5241f52438
boson13579 <boson13579@gmail.com>
2023-01-16 22:28:55 +0800

    pratice

--- C++/CSES/Dynamic_Programming/Dice_Combinations/Dice_Combinations.cpp
+++ C++/CSES/Dynamic_Programming/Dice_Combinations/Dice_Combinations.cpp
@@ -1,36 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-DP[1000005];
+int DP[1000005];
 
-int dfs(int n){
+int dfs(int n) {
+    if (DP[n] != 0 or n == 0) return DP[n];
 
-	if(DP[n]!=0 or n==0)	return DP[n];
+    int sum = 0;
 
-	int sum=0;
-
-	for(int i=1 ; i<7;i++){
-		
-	}
-
-
+    for (int i = 1; i <= min(n, 6); i++) {
+        DP[n - i] = dfs(n - i);
+        sum += DP[n - i];
+        sum %= 1000000007;
+    }
+    DP[n] == sum;
+    return sum;
 }
 
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
 
-int main () {
-	//freopen("test.in", "r", stdin);
-	//freopen("test.out","w", stdout);
-	ios::sync_with_stdio(0), cin.tie(0);
+    DP[0] = 1;
+    DP[1] = 1;
+    DP[2] = 2;
 
-	DP[0] = 0;
-	DP[1] = 1;
-	DP[2] = 2;
-	
-	int a;
-	cin>>a;
-	
+    int a;
+    cin >> a;
+    cout << dfs(a) << "\n";
 
-
-	return 0;
+    return 0;
 }
-
