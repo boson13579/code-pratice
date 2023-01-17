0de30c2d70b5993f351c4cc8dfdc0113082595ba
boson13579 <boson13579@gmail.com>
2023-01-18 02:32:57 +0800

    p

--- C++/CSES/Sorting/Collecting_Numbers/Collecting_Numbers.cpp
+++ C++/CSES/Sorting/Collecting_Numbers/Collecting_Numbers.cpp
@@ -1,16 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<pair<int, int>> arr;
 
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
 
-int main () {
-	//freopen("test.in", "r", stdin);
-	//freopen("test.out","w", stdout);
-	ios::sync_with_stdio(0), cin.tie(0);
+    int n, temp, ans = 1;
+    cin >> n;
 
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+        arr.emplace_back(make_pair(temp, i));
+    }
 
+    sort(arr.begin(), arr.end());
 
+    for (int i = 0; i < n - 1; i++)
+        if (arr[i].second > arr[i + 1].second) ans++;
 
-	return 0;
+    cout << ans << "\n";
+
+    return 0;
 }
-
--- test.cpp
+++ test.cpp
@@ -1,11 +1,35 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-
-set<int> q;
-
 int main() {
-	int a;
-	cout<<*q.begin()<<endl;
-	return 0;
+    long long input;
+    cin >> input;
+    if (input * (input + 1) / 2 % 2 != 0)
+        cout << "NO\n";
+    else {
+        if (input % 4 == 0) {
+            cout << "YES " << input / 2 << " ";
+            for (long long i = 1; i < input / 2; i += 2) cout << i << " ";
+            for (long long i = input / 2 + 2; i < input + 1; i += 2)
+                cout << i << " ";
+            cout << input / 2 << " ";
+            for (long long i = 2; i < input / 2 + 1; i += 2) cout << i << " ";
+            for (long long i = input / 2 + 1; i < input + 1; i += 2)
+                cout << i << " ";
+            cout << "\n";
+        } else if (input % 4 == 1 || input % 4 == 2)
+            cout << "NO\n";
+        else if (input % 4 == 3) {
+            cout << "YES " << (input + 1) / 2 << " ";
+            for (long long i = 1; i < (input + 1) / 2; i += 2) cout << i << " ";
+            for (long long i = (input + 1) / 2; i < input + 1; i += 2)
+                cout << i << " ";
+            cout << (input - 1) / 2 << " ";
+            for (long long i = 2; i < (input - 1) / 2; i += 2) cout << i << " ";
+            for (long long i = (input + 1) / 2 + 1; i < input + 1; i += 2)
+                cout << i << " ";
+            cout << "\n";
+        }
+    }
+    return 0;
 }
