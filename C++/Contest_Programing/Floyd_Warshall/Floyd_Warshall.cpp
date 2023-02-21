59edca4178223234ad08211a0c7a724457c28f97
boson13579 <boson13579@gmail.com>
2023-02-22 02:30:05 +0800

    continue...

--- C++/Contest_Programing/Floyd_Warshall/Floyd_Warshall.cpp
+++ C++/Contest_Programing/Floyd_Warshall/Floyd_Warshall.cpp
@@ -17,20 +17,22 @@ int main() {
 
 	for (int i = 0, a, b, c; i < k; i++) {
 		cin >> a >> b >> c;
-		if (dis[a][b] <= c) continue;
-		for (int fr = 1; fr <= n; fr++) {
-			for (int to = 1; to <= n; to++) {
-				if (fr == to) continue;
-				if (dis[fr][a] + c + dis[b][to] < dis[fr][to]) {
-					dis[fr][to] = dis[fr][a] + c + dis[b][to];
-					dis[to][fr] = dis[fr][to];
-				}
-				if (dis[fr][b] + c + dis[a][to] < dis[fr][to]) {
-					dis[fr][to] = dis[fr][b] + c + dis[a][to];
-					dis[to][fr] = dis[fr][to];
+		if (dis[a][b] > c) {
+			for (int fr = 1; fr <= n; fr++) {
+				for (int to = 1; to <= n; to++) {
+					if (fr == to) continue;
+					if (dis[fr][a] + c + dis[b][to] < dis[fr][to]) {
+						dis[fr][to] = dis[fr][a] + c + dis[b][to];
+						dis[to][fr] = dis[fr][to];
+					}
+					if (dis[fr][b] + c + dis[a][to] < dis[fr][to]) {
+						dis[fr][to] = dis[fr][b] + c + dis[a][to];
+						dis[to][fr] = dis[fr][to];
+					}
 				}
 			}
 		}
+
 		long long ans = 0;
 		for (int i = 1; i <= n; i++) {
 			for (int t = i + 1; t <= n; t++) {
