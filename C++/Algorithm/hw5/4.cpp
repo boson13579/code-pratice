1151796b1644c3b42d07932c0ad729da4f345f3f
boson13579 <boson13579@gmail.com>
2023-03-27 20:52:49 +0800

    practice

---
+++ C++/Algorithm/hw5/4.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s1 = "ACTGCCTAGCG";
+string s2 = "TCAGGTCGA"
+
+DP[][]
+
+
+
+
+int main () {
+	ios::sync_with_stdio(0), cin.tie(0);
+
+
+
+
+	return 0;
+}
+
--- C++/data_struct/Postfix/Posfix.cpp
+++ C++/data_struct/Postfix/Posfix.cpp
@@ -14,8 +14,6 @@ map<string, int> icp{{"(", 20}, {")", 19}, {"+", 12}, {"-", 12},
 void stackExecute(string mystack);
 
 int main() {
-    // freopen("test.in", "r", stdin);
-    // freopen("test.out","w", stdout);
     ios::sync_with_stdio(0), cin.tie(0);
 
     cin >> a;
